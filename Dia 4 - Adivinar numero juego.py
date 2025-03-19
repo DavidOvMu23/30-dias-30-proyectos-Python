4fae93b49246230b675a345700e9eb9c9b8651c4
David O <david23oviedo@gmail.com>
2025-03-19 19:37:00 +0100

    Dia 4 V2

--- Dia 4 - Adivinar numero juego.py
+++ Dia 4 - Adivinar numero juego.py
@@ -1,7 +1,6 @@
 import random
 
 numero_entero = random.randint(0, 100)
-print(f"Número entero aleatorio: {numero_entero}")
 
 intentos = 0
 
@@ -9,11 +8,14 @@ while True:  # El bucle se ejecuta hasta que se encuentra un break
     intentos += 1  # Contador de intentos
     numero_usuario = int(input("Adivina el número: "))
     if numero_usuario == numero_entero:
+        print("")
         print(f"¡Felicidades! Adivinaste el número en {intentos} intentos.")
         break  # Rompe el bucle y termina el programa
 
     elif numero_usuario < numero_entero:
+        print("")
         print("El número es mayor.")
 
     else:
+        print("")
         print("El número es menor.")
